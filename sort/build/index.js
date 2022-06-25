a2516651d5f7a4a130389cb2567e969bd215e3dd
Zoey Kim <devzoeykim@gmail.com>
2022-06-25 10:41:17 +0900

    Sorter 문자열 추가

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,21 +1,10 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, 5, -2, 0]);
+const charactersCollections = new CharactersCollection_1.CharactersCollection("spwreDF");
+const sorter = new Sorter_1.Sorter(charactersCollections);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollections.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,32 +1,9 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // ===== collection이 숫자 배열일 때: number[]
-        // this.collection instanceof Array: 여기서 작업하려는 collection이 배열임을 보장해줌
-        // => Type Guards
-        // instanceof: cunstructor function에 의해 생성된 값의 타입
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        // ===== collection이 문자열일 때: string
-        // typeof: number, string, boolean, symbol 등 원시 타입
-        if (typeof this.collection === "string") {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, 5, -2, 0]);
+const charactersCollections = new CharactersCollection("spwreDF");
+const sorter = new Sorter(charactersCollections);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollections.data);
