904990b855aa901ab1c407e6fdd788ab75179c37
Zoey Kim <devzoeykim@gmail.com>
2022-06-25 14:30:24 +0900

    stats 초기 셋업

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,19 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10, 3, 5, -2, 0]);
-const charactersCollections = new CharactersCollection("spwreDF");
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(10);
-linkedList.add(-5);
-linkedList.add(4);
+// const numbersCollection = new NumbersCollection([10, 3, 5, -2, 0]);
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+// const charactersCollection = new CharactersCollection("SDfvDdhyu");
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(5);
+linkedList.add(10);
+linkedList.add(0);
+linkedList.add(-9);
+linkedList.sort();
 linkedList.print();
